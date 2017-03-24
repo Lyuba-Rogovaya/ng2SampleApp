6d2687e9170705aad773618c1d231f6791c3f2a7
Lyuba-Rogovaya <lyubov.rogovaya@gmail.com>
2017-03-24 18:44:29 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>
-    {{title}}
-  </h1>
-  `,
-  styles: []
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'app works!';
+
 }
